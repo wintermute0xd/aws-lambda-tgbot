c076e0daa1b2dbad14823ed1225e7beed7b6acad
wintermute0xd <v110113@gmail.com>
2022-07-31 21:07:57 +0200

    init test

---
+++ aws-lambda-tgbot.py
@@ -0,0 +1,15 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    # try:
+    #     response = s3_client.upload_file(file_name, bucket, object_name)
+    # except ClientError as e:
+    #     logging.error(e)
+    #     return False
+    # return True
+
+    with open("file", "rb") as f:
+        f.write = "test123"
+        s3.upload_fileobj(f, "vedro-0", "test-lambda.txt")
